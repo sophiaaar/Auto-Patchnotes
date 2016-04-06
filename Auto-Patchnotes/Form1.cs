c37a06a4f473fa4b746665475dbad5c61390fe90
Sophia Clarke <sophiaclarke93@live.com>
2016-04-06 22:17:30 +0100

    Added the main form. This is the patchnote generator at its most simple level (ie just perforce in //depot/). Will eventually modify this tool so the user can customise it without having to go into the code.

--- Auto-Patchnotes/Form1.cs
+++ Auto-Patchnotes/Form1.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace Auto_Patchnotes
+namespace PatchnoteGenerator
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ParseNotes(bool fullNotes)
+        {
+            slimNotesButton.Enabled = false;
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo outputDir = new DirectoryInfo(currentDirectory + @"\Patchnotes");
+
+            if (!outputDir.Exists)
+            {
+                outputDir.Create();
+            }
+
+            int parsedValue;
+            if (!int.TryParse(changelistTextBox1.Text, out parsedValue))
+            {
+                MessageBox.Show("Changelist 1 must be a number");
+                slimNotesButton.Enabled = true;
+                return;
+            }
+
+            if (changelistTextBox1.Text.Length != 6)
+            {
+                MessageBox.Show("Changelist must be 6 digits long");
+                slimNotesButton.Enabled = true;
+                return;
+            }
+
+            if (changelistTextBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Changelist cannot be blank");
+                slimNotesButton.Enabled = true;
+                return;
+            }
+
+            if (changelistTextBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Changelist cannot be blank");
+                slimNotesButton.Enabled = true;
+                return;
+            }
+
+            // A regex to match the format of your branches. Can be changed, or not used
+            string pattern = "^\\d{1}-\\d{2}-\\d{1}/\\d{1}-\\d{2}-\\d{1}";
+
+            if (branchTextBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter a branch");
+                slimNotesButton.Enabled = true;
+                return;
+            }
+
+            string branch;
+
+            if (Regex.IsMatch(branchTextBox1.Text, pattern))
+            {
+                branch = @"//depot/" + branchTextBox1.Text;
+                PatchnoteGenerator.StartParse(changelistTextBox1.Text, changelistTextBox2.Text, branch);
+            }
+            else
+            {
+                MessageBox.Show(@"Enter branch in the format X-XX-X/X-XX-X_NAME.", "Invalid branch");
+            }
+
+            slimNotesButton.Enabled = true;
+        }
+
+        private void slimNotesButton_Click(object sender, EventArgs e)
+        {
+            ParseNotes(false);
         }
     }
 }
--- Auto-Patchnotes/Program.cs
+++ Auto-Patchnotes/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Auto_Patchnotes
+namespace PatchnoteGenerator
 {
     static class Program
     {
